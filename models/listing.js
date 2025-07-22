be9c73c65f74b25fbb16dccceb82c23662473916
Afsar0Ahmed <sk4885426@gmail.com>
2025-07-22 19:52:26 +0530

    Initial commit

--- models/listing.js
+++ models/listing.js
@@ -2,16 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
+  title: String,
   description: String,
-  image:String,
   price: Number,
+  image: String,
   location: String,
-  country: String,
+  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }]
 });
 
-const Listing = mongoose.model("Listing", listingSchema);
-module.exports = Listing;
+module.exports = mongoose.model("Listing", listingSchema);
